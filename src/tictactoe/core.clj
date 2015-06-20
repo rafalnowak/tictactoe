039e6b6ba397ab24a409d61dd16e92378a2d5d88
Rafał Nowak <rafal.nowak1@gmail.com>
2015-06-20 23:31:06 +0200

    Simple structures

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,7 +1,25 @@
 (ns tictactoe.core
   (:gen-class))
 
+(def cross "X")
+(def circle "O")
+(def empty-field " ")
+
+(defrecord Board [size fields])
+
+(defn empty-board [size] (let [empty-fields (zipmap (range 0 (* size size)) (repeat (* size size) empty-field))]
+                           (->Board size empty-fields)))
+
+(defn board-fields-count [board] (let [size (:size board)]
+                                   (* size size)))
+
+(defn coords-to-index [board row col] (+ col (* row (:size board))))
+(defn get-board-elem [board row col] (get (:fields board) (coords-to-index board row col)))
+
+(defn put-field [board row col value] (->Board (:size board) (assoc (:fields board) (coords-to-index board row col) value)))
+(defn put-cross [board row col] (put-field board row col cross))
+(defn put-circle [board row col] (put-field board row col circle))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
