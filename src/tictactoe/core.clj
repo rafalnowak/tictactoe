fbe8b7e0fafce76d35ce390ae0f10bd33e3c1af3
Rafał Nowak <rafal.nowak1@gmail.com>
2015-06-21 01:36:15 +0200

    Formatting and more funcitons.

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -6,20 +6,34 @@
 (def empty-field " ")
 
 (defrecord Board [size fields])
+(defrecord MoveResult [board moved])
 
-(defn empty-board [size] (let [empty-fields (zipmap (range 0 (* size size)) (repeat (* size size) empty-field))]
-                           (->Board size empty-fields)))
+(defn empty-board [size]
+  (let [empty-fields (zipmap (range 0 (* size size)) (repeat (* size size) empty-field))]
+    (->Board size empty-fields)))
 
-(defn board-fields-count [board] (let [size (:size board)]
-                                   (* size size)))
+(defn board-fields-count [board]
+  (let [size (:size board)]
+    (* size size)))
 
-(defn coords-to-index [board row col] (+ col (* row (:size board))))
-(defn get-board-elem [board row col] (get (:fields board) (coords-to-index board row col)))
+(defn coords-to-index [board row col]
+  (+ col (* row (:size board))))
+(defn get-board-elem [board row col]
+  (get (:fields board) (coords-to-index board row col)))
 
-(defn put-field [board row col value] (->Board (:size board) (assoc (:fields board) (coords-to-index board row col) value)))
-(defn put-cross [board row col] (put-field board row col cross))
-(defn put-circle [board row col] (put-field board row col circle))
+
+(defn is-empty-field [board row col]
+  (let [index (coords-to-index board row col)]
+    (= empty-field (get (:fields board) index))))
+
+(defn put-field [board row col value]
+  (->Board (:size board) (assoc (:fields board) (coords-to-index board row col) value)))
+(defn put-cross [board row col]
+  (put-field board row col cross))
+(defn put-circle [board row col]
+  (put-field board row col circle))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [start-board (empty-board 3)]
+    (println start-board)))
