410f6521c73c6578c889b506453b4d11edac2bf1
Rafał Nowak <rafal.nowak1@gmail.com>
2015-06-29 19:43:51 +0200

    Another simple test.

--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -7,3 +7,9 @@
     (let [board (empty-board 3)
           size (count (keys (:fields board)))]
       (is (= 9 size)))))
+
+(deftest board-put
+  (testing "Board should have field changed after putting cross"
+    (let [board-after-move (put-cross (empty-board 3) 1 1)
+          board (:board board-after-move)]
+      (is (= cross (get-board-elem board 1 1))))))
