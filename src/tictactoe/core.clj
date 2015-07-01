46dcb03bbbecc69af6526d5b48e67bd6e16751d8
Rafał Nowak <rafal.nowak1@gmail.com>
2015-07-01 22:40:59 +0200

    Game loop with move checking.

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -6,26 +6,36 @@
 
 (defrecord Movement [mark row col])
 
-(defn movement 
-  [mark movement-string]
-  (let [coords (str/split movement-string #" ")
-        row (Integer/parseInt (get coords 0))
-        col (Integer/parseInt (get coords 1))]
-          (->Movement mark row col)))
-
 (defn prompt-move
   []
   (println "Enter coordinates for move (row col): ")
   (let [input (str/trim (read-line))]
     input))
 
+(defn create-movement 
+  [mark movement-string]
+  (let [coords (str/split movement-string #" ")
+        row (Integer/parseInt (get coords 0))
+        col (Integer/parseInt (get coords 1))]
+          (->Movement mark row col)))
+
+(defn attemp-move
+  [board]
+  (let [move-coords (prompt-move)
+        player-movement (create-movement board/cross move-coords)
+        row (:row player-movement)
+        col (:col player-movement)]
+          (if (board/is-field-empty? board row col) 
+            (board/put-cross board row col)
+            (do
+              (println "Illegal move, try again") 
+              (recur board)))))
+
 (defn game-loop
   [board]
-  (let [move (prompt-move)
-        player-movement (movement board/cross move)
-        board-after-move (board/put-cross board (:row player-movement) (:col player-movement))]
-          (board/print-board board-after-move)
-          (recur board-after-move)))
+    (let [board-after-move (attemp-move board)]
+      (board/print-board board-after-move)
+      (recur board-after-move)))
 
 (defn -main
   [& args]
