73a39c6da79647cc618a183ed341de118916ac39
Rafał Nowak <rafal.nowak1@gmail.com>
2015-07-05 20:07:07 +0200

    Gameplay with player change.

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -10,11 +10,26 @@
 (def o-win "O-WIN")
 
 (defrecord Movement [mark row col])
-(defrecord BoardStatus [board game-status])
+(defrecord GameState [board game-status player-to-move])
+(defrecord Player [mark])
 
-(defn prompt-move
-  []
-  (println "Enter coordinates for move (row col): ")
+;; TODO: stronger coupling with player so no if will be necessary
+(defn next-player
+  [player]
+  (if (= board/cross (:mark player))
+    (->Player board/circle)
+    (->Player board/cross)))
+
+;; TODO: stronger coupling with player so no if will be necessary
+(defn winning-status-for-player
+  [player]
+  (if (= board/cross (:mark player))
+    x-win
+    o-win))
+
+(defn prompt-for-move
+  [player]
+  (println (str (:mark player) " - enter coordinates for move (row col): "))
   (let [input (str/trim (read-line))]
     input))
 
@@ -25,37 +40,38 @@
         col (Integer/parseInt (get coords 1))]
           (->Movement mark row col)))
 
-;; TODO: parametrized players symbol and players changing
 (defn attemp-move
-  [board]
-  (let [move-coords (prompt-move)
-        player-movement (create-movement board/cross move-coords)
+  [board player]
+  (let [move-coords (prompt-for-move player)
+        player-movement (create-movement (:mark player) move-coords)
         row (:row player-movement)
         col (:col player-movement)]
           (if (board/is-field-empty? board row col) 
-            (let [board (board/put-cross board row col)]
-              (if (board/check-if-win? board board/cross)
-                (->BoardStatus board x-win) 
-                (->BoardStatus board game-running))) 
+            (let [board (board/put-field board row col (:mark player))]
+              (if (board/check-if-win? board (:mark player))
+                (->GameState board (winning-status-for-player player) player) 
+                (->GameState board game-running player))) 
             (do
               (println "Illegal move, try again") 
-              (recur board)))))
+              (recur board player)))))
 
 (defn game-loop
-  [board]
-    (let [board-after-move (attemp-move board)
+  [board player]
+    (let [board-after-move (attemp-move board player)
           board (:board board-after-move)
           status (:game-status board-after-move)]
-      (if (= x-win status)
+      (if (or (= x-win status) (= o-win status))
         (do 
           (board/print-board board)
           (println (str "Game over! " status)))
         (do
           (board/print-board board)
-          (recur board)))))
+          (recur board (next-player player))))))
 
+;; TODO: random starting player
 (defn -main
   [& args]
-  (let [start-board (board/empty-board 3)]
+  (let [start-board (board/empty-board 3)
+        starting-player (->Player board/circle)]
     (board/print-board start-board)
-    (game-loop start-board)))
+    (game-loop start-board starting-player)))
